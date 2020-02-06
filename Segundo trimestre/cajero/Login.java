e973f4e508abada8898c1f8f92943bf69df0cf26
DeltaMurillo <55785615+DeltaMurillo@users.noreply.github.com>
2020-02-06 09:55:01 +0100

    Add files via upload

---
+++ Segundo trimestre/cajero/Login.java
@@ -0,0 +1,18 @@
+package cajero;
+import cajero.data.Cuenta;
+import cajero.data.datos;
+
+import java.util.Scanner;
+
+public class Login {
+    static Scanner key = new Scanner(System.in);
+    private static int loginID;
+    private static int loginPin;
+    public static void main(String[] args) {
+    }
+
+    public void getUsername(){
+        datos nombre = new datos();
+        System.out.println("Bienvenido, introduce tu ID.");
+    }
+}
